b9a1df66525e047cd6df753d0e2e4d79e09dd5be
JuanCarlos1196 <juan1196mtz@gmail.com>
2019-02-06 09:59:53 -0600

    project
    fg

---
+++ reposi/reposi/Laptop.swift
@@ -0,0 +1,13 @@
+//
+//  Laptop.swift
+//  reposi
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 06/02/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class Laptop: NSObject {
+
+}
